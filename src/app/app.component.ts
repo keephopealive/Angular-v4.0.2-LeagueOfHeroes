1b27f97e48c68a855e11b5c7563eb25f73bcbc52
Speros Misirlakis <smisirlakis@gmail.com>
2017-06-16 15:30:09 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  characterSelect: boolean = true;
+  playerOne = null;
+  playerTwo = null;
+  playerOneHealth = 25;
+  playerTwoHealth = 25;
+  currentTurn = 1;
+
+  playerOneCharacter(selection, event) {
+    this.playerOne = selection;
+  }
+
+  playerTwoCharacter(selection) {
+    this.playerTwo = selection;
+  }
+
+  battleCheck() {
+    if (this.playerOne !== null && this.playerTwo !== null) {
+      alert('Lets Battle!');
+      this.characterSelect = false;
+    }
+  }
+
+  attack(player: string) {
+    this.nextTurn()
+    const dmg = Math.floor(Math.random() * 10) + 1;
+    if (player === 'playerOne') {
+     this.playerOneHealth -= dmg;
+    } else {
+      this.playerTwoHealth -= dmg;
+    }
+    this.checkWin();
+  }
+
+  nextTurn() {
+    if (this.currentTurn === 1) {
+      this.currentTurn = 2;
+    } else {
+      this.currentTurn = 1;
+    }
+  }
+
+  heal(player: string) {
+    this.nextTurn();
+    const dmg = Math.floor(Math.random() * 7) + 1;
+    if (player === 'playerOne') {
+     this.playerOneHealth += dmg;
+    } else {
+      this.playerTwoHealth += dmg;
+    }
+  }
+
+  checkWin() {
+    if (this.playerOneHealth <= 0) {
+      alert(this.playerTwo + ' Wins!');
+      this.reset();
+    } else if (this.playerTwoHealth <= 0) {
+      alert(this.playerOne + ' Wins!');
+      this.reset();
+    }
+  }
+
+  reset() {
+    this.playerOne = null;
+    this.playerTwo = null;
+    this.characterSelect = true;
+    this.playerOneHealth = 25;
+    this.playerTwoHealth = 25;
+  }
+
 }
